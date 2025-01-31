1aefa3713d723371511579f8d3eff53753fd2a53
Azamat Nigmatullin <ngmazamat@gmail.com>
2025-01-31 18:25:15 +0400

    minor

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export function frodo(a) {
+export function frodo() {
   console.log("Хоббитон");
   console.log("Могильные Холмы");
   console.log("Бри");
@@ -15,5 +15,4 @@ export function frodo(a) {
   console.log("Логово Шелоб");
   console.log("Кирит Унгол");
   console.log("Роковая Гора");
-  return a;
 }
