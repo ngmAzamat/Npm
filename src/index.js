8c01d1910a31cda6438fb6d420f665db2f981cfd
Azamat Nigmatullin <ngmazamat@gmail.com>
2025-01-31 17:09:33 +0400

    commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export function frodo(a) {
+  console.log("Хоббитон");
+  console.log("Могильные Холмы");
+  console.log("Бри");
+  console.log("Амон сул");
+  console.log("Ривенделл");
+  console.log("Верхный перевал");
+  console.log("Мория");
+  console.log("Лотлориэн");
+  console.log("Амон хен");
+  console.log("Мертвые топи");
+  console.log("Мораннон");
+  console.log("Итилен");
+  console.log("Осгилиат");
+  console.log("Логово Шелоб");
+  console.log("Кирит Унгол");
+  console.log("Роковая Гора");
+  return a;
+}
